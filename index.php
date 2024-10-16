8925e4559a9aa379bcc682389f2c4dee4914b752
sylsylviane <syl.sylviane1@gmail.com>
2024-10-16 16:33:38 -0400

    Ajout de la boucle wp pour afficher l'ensemble des articles et début de structure Sass

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
         <?php wp_head(); ?>
     </head>
     <body>
-        <header>
+        <header class="entete">
             <section class="global">
                 <h1>31w</h1>
                 <nav>
@@ -27,13 +27,26 @@
                 </form>
             </section>
         </header>
-        <main>
+        <main class="principal">
             <section class="global">
                 <h2>Accueil</h2>
+                <?php if (have_posts()): ?> <!-- VERIFIER SI AU MOINS UN ARTICLE EST EXTRAIT ???-->
+                    <?php while (have_posts()): the_post(); ?> 
+                    <?php
+                        $chaine = get_the_title();
+                        $sigle = substr($chaine, 0, 7);
+                        $titre = substr($chaine, 8, 40);
+                    ?>
+                    <article class="principal__article">
+                        <h3><?php echo $sigle; ?></h3> <!--retourne le titre -->
+                        <p><?php echo wp_trim_words(get_the_excerpt(), 20, null); ?></p> <!--the_excerpt fait un echo, get_the_experpt retourne une chaine -->
+                    </article>
+                    <?php endwhile; ?>
+                <?php endif; ?>
                 <p>Bienvenue sur 31w Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto autem voluptatem, ipsum velit dolores aliquam ex explicabo atque at vero qui illum possimus obcaecati odit cupiditate aliquid iure repellat dolore!</p>
             </section>
         </main>    
-        <footer>
+        <footer class="pied">
             <section class="global">
                 <div>1</div>
                 <div>2</div>
