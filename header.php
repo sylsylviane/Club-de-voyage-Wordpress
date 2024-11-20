2814912ca93ef997dfc1fb585ab7282ad1e9b4f1
sylsylviane <syl.sylviane1@gmail.com>
2024-11-20 15:58:47 -0500

    20/11/24 Ajouter logo dans header.php

--- header.php
+++ header.php
@@ -18,20 +18,28 @@
 
 <body>
     <header class="entete">
+        
         <section class="global entete__global">
             <div class="entete__titre">
-                <h1><a href="<?php bloginfo('url') ?>"><?php bloginfo('name') ?></a></h1>
-                <h2><?php bloginfo('description') ?></h2>
-            </div>    
+                <?php
+        if (function_exists('the_custom_logo')) {
+            the_custom_logo();
+        } else {
+            echo '<a href="' . esc_url(home_url('/')) . '">' . get_bloginfo('name') . '</a>';
+        }
+        ?>
+                <!-- <h1><a href="<?php bloginfo('url') ?>"><?php bloginfo('name') ?></a></h1>
+                <h2><?php bloginfo('description') ?></h2> -->
+            </div>
             <div class="entete__nav">
                 <?php wp_nav_menu(
                     array(
                         "menu" => "principal",
                         "container" => "nav"
                     )
-                ); 
-                get_search_form(); 
+                );
+                get_search_form();
                 ?>
-            </div>    
+            </div>
         </section>
     </header>
