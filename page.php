2b7c0dadb0f0de0ed089e1b2439b54e44c0114af
sylsylviane <syl.sylviane1@gmail.com>
2024-11-20 15:59:53 -0500

    Ajouter style pour logo

---
+++ page.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * index.php - Le modèle par défaut de wordpress
+ */
+?>
+<?php get_header() ?>
+
+<main class="principal">
+  <section class="global">
+    <h2>Contenu d'une page - page.php</h2>
+    <div class="principal__conteneur">
+      <?php if (have_posts()): ?>
+        <?php while (have_posts()) :  the_post(); ?>
+          <article class="principal__article">
+            <h5><?php the_title() ?></h5>
+            <p><?php the_content() ?></p>
+          </article>
+        <?php endwhile; ?>
+    </div>
+
+  <?php endif ?>
+  </section>
+</main>
+<?php get_footer() ?> 
