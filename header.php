a5689ed5819e7309c3f2c8f67bee51185a1d5c9f
sylsylviane <syl.sylviane1@gmail.com>
2024-11-28 18:24:33 -0500

    Menu toggle

--- header.php
+++ header.php
@@ -13,25 +13,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>31W</title>
     <link href="https://cdn.jsdelivr.net/npm/remixicon@3.2.0/fonts/remixicon.css" rel="stylesheet">
+
     <?php wp_head(); ?>
 </head>
 
 <body>
     <header class="entete">
-        
+
         <section class="global entete__global">
             <div class="entete__titre">
+                
                 <?php
-        if (function_exists('the_custom_logo')) {
-            the_custom_logo();
-        } else {
-            echo '<a href="' . esc_url(home_url('/')) . '">' . get_bloginfo('name') . '</a>';
-        }
-        ?>
+                if (function_exists('the_custom_logo')) {
+                    the_custom_logo();
+                } else {
+                    echo '<a href="' . esc_url(home_url('/')) . '">' . get_bloginfo('name') . '</a>';
+                }
+                ?>
                 <!-- <h1><a href="<?php bloginfo('url') ?>"><?php bloginfo('name') ?></a></h1>
                 <h2><?php bloginfo('description') ?></h2> -->
             </div>
             <div class="entete__nav">
+                    <input type="checkbox" id="menu-toggle" class="menu-toggle" />
+                <label
+                    for="menu-toggle"
+                    class="menu-bouton-conteneur"
+                    aria-label="menu-toggle">
+                    <span><i class="ri-menu-line"></i></span>
+                </label>
                 <?php wp_nav_menu(
                     array(
                         "menu" => "principal",
@@ -40,6 +49,8 @@
                 );
                 get_search_form();
                 ?>
+
             </div>
+
         </section>
     </header>
