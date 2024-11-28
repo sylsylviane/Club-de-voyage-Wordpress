5f1a4c8b7a13e542bbcdf2a2316ee8bde1bedaa4
sylsylviane <syl.sylviane1@gmail.com>
2024-11-28 18:28:10 -0500

    28/11/24 Modifier paragraphe

--- 404.php
+++ 404.php
@@ -8,11 +8,10 @@
 
 <main class="principal">
     <section class="global">
-        <code>404.php</code>
         <div class="principal__conteneur principal__erreur404">
             <h2 class="page-title"><?php esc_html_e('Oops! Cette page ne peut pas être trouvée'); ?></h2>
             <div class="page-content">
-                <p><?php esc_html_e('Il semble que rien n’ait été trouvé à cet endroit. Peut-être essayer une recherche parmi les cours ci-dessous ?'); ?></p>
+                <p><?php esc_html_e('Il semble que rien n’ait été trouvé à cet endroit. Peut-être essayer une recherche parmi les catégories ci-dessous ?'); ?></p>
                 <?php
                 wp_nav_menu(array(
                     "menu" => "erreur404",
