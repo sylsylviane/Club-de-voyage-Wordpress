bcd5d8dc1f4229f749982013080a35498b99008a
sylsylviane <syl.sylviane1@gmail.com>
2024-12-18 16:06:01 -0500

    Créer template-parts pour formulaire d'inscription

---
+++ template-parts/inscription.php
@@ -0,0 +1,30 @@
+<!-- ============================================================= -->
+
+  <section id="inscription" class="global form-inscription">
+    <form action="">
+      <div>
+        <label for="">Nom
+        <input type="text" name="nom" placeholder="Ecrivez votre nom">
+      </label>
+      </div>
+      <div>
+        <label for="">Prénom
+        <input type="text" name="prenom" placeholder="Ecrivez votre prénom">
+      </label>
+      </div>
+      <div>
+        <label for="">Courriel
+        <input type="email" name="courriel" placeholder="Ecrivez votre courriel">
+      </label>
+      </div>
+      <div>
+        <label for="">Téléphone
+        <input type="text" name="telephone" placeholder="Ecrivez votre téléphone">
+      </label>
+      </div>
+       <div>
+        <input type="submit" value="S'inscrire">
+       </div>
+    </form>
+  </section>
+  <!-- ============================================================== -->
