3a5e8244fb54bf54f344ee54d187e79d7888ebf7
sylsylviane <syl.sylviane1@gmail.com>
2024-12-19 16:20:49 -0500

    Ajouter un permalink

--- category.php
+++ category.php
@@ -6,22 +6,27 @@
 ?>
 <?php get_header() ?>
 
-<!-- Permet d'obtenir toutes les infos sur la catégorie active, y compris la description -->
-  <?php $category = get_queried_object();
-  if (isset($category->description)){
-    echo esc_html($category->description);
-  } ?>
-  <!-- ========================================================= -->
-   
+
+<!-- ========================================================= -->
+
 <main class="principal">
+  <!-- Permet d'obtenir toutes les infos sur la catégorie active, y compris la description -->
+  <section class="principal__description">
+    <p> <?php $category = get_queried_object();
+        if (isset($category->description)) {
+          echo esc_html($category->description);
+        } ?></p>
+  </section>
   <section class="global">
     <h2><?php single_cat_title() ?></h2>
     <div class="principal__conteneur">
       <?php if (have_posts()): ?>
         <?php while (have_posts()) :  the_post(); ?>
           <article class="principal__article">
+            <?php the_post_thumbnail('medium', ['class' => 'carte-img-top', 'alt' => 'Image de voyage']) ?>
             <h5><?php the_title() ?></h5>
             <p><?php the_content() ?></p>
+            <a href="<?php the_permalink() ?>" class="bouton">Voir plus</a>
           </article>
         <?php endwhile; ?>
     </div>
@@ -29,4 +34,4 @@
   <?php endif ?>
   </section>
 </main>
-<?php get_footer() ?> 
+<?php get_footer() ?>
