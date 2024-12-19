ad7bc631585269c5074b82f932e02e42d68085a7
sylsylviane <syl.sylviane1@gmail.com>
2024-12-19 16:23:29 -0500

    Formatter fichier single

--- single.php
+++ single.php
@@ -8,16 +8,18 @@
 
 <main class="principal">
   <section class="global">
+
     <div class="principal__conteneur">
       <?php if (have_posts()): ?>
         <?php while (have_posts()) :  the_post(); ?>
           <article class="principal__article">
-            <h1><?php  the_title() ?></h1>
+            <h1><?php the_title() ?></h1>
             <figure class="article__figure">
-            <?php if (has_post_thumbnail()) {
-                  the_post_thumbnail('large', ['class' => 'bigImg', 'alt' => 'Image de voyage' ]); } ?>
-                  <!-- <figcaption>figcaption</figcaption> -->
-              </figure>
+              <?php if (has_post_thumbnail()) {
+                the_post_thumbnail('large', ['class' => 'bigImg', 'alt' => 'Image de voyage']);
+              } ?>
+              <figcaption><?php the_title() ?></figcaption>
+            </figure>
             <?php the_content() ?>
           </article>
         <?php endwhile; ?>
